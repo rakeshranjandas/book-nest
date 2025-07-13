69e333afdf7c16a764c429c89553b8412379a7f8
rakeshranjandas <rakeshranjandas.bbsr93@gmail.com>
2025-07-13 17:28:31 +0530

    Edit book details

--- src/routes/private/books/[bookId]/+page.svelte
+++ src/routes/private/books/[bookId]/+page.svelte
@@ -13,41 +13,76 @@
 
     let {data}: BookPageProps = $props();
     let book = $derived(data.book);
+    let isEditMode = $state(false);
+
+    let title = $state(book.title);
+    let author = $state(book.author);
+    let description = $state(book.description || "");
+    let genre = $state(book.genre || "");
 
     function goBack() {
         history.back();
     }
+
+    function toggleEditMode() {
+        isEditMode = !isEditMode;
+    }
 </script>
 
 {#snippet bookInfo()}
-    
-<h2 class="book-title mb-m">{book.title}</h2>
-<p class="book-author">by {book.author}</p>
-<h4 class="mt-m mb-xs semi-bold">Your rating</h4>
-<StarRating value={book.rating || 0} />
-<p class="small-font">
-    Click to {book.rating ? "change": "give"} rating
-</p>
-{#if book.description}
-    <h4 class="mt-m mb-xs semi-bold">Description</h4>
-    <p class="mb-m">{book.description}</p>
-{:else}
-<h4 class="mt-m mb-xs semi-bold">No description yet.</h4>
-<button class="block mb-m" onclick={() => console.log("toggle on the edit mode")}>
-    <p>Click to add one</p>
-</button>
-{/if}
+    <h2 class="book-title mb-m">{book.title}</h2>
+    <p class="book-author">by {book.author}</p>
+    <h4 class="mt-m mb-xs semi-bold">Your rating</h4>
+    <StarRating value={book.rating || 0} />
+    <p class="small-font">
+        Click to {book.rating ? "change": "give"} rating
+    </p>
+    {#if book.description}
+        <h4 class="mt-m mb-xs semi-bold">Description</h4>
+        <p class="mb-m">{book.description}</p>
+    {:else}
+    <h4 class="mt-m mb-xs semi-bold">No description yet.</h4>
+    <button class="block mb-m" onclick={() => console.log("toggle on the edit mode")}>
+        <p>Click to add one</p>
+    </button>
+    {/if}
 
-{#if !book.finished_reading_on}
-<Button isSecondary={true} onclick={() => console.log("Updating reading status")}>
-    {book.started_reading_on ? "I finished reading this book": "I started reading this book"}
-</Button>
-{/if}
+    {#if !book.finished_reading_on}
+    <Button isSecondary={true} onclick={() => console.log("Updating reading status")}>
+        {book.started_reading_on ? "I finished reading this book": "I started reading this book"}
+    </Button>
+    {/if}
 
-{#if book.genre}
-<h4 class="mt-m mb-xs semi-bold">Genre</h4>
-<p>{book.genre}</p>
-{/if}
+    {#if book.genre}
+    <h4 class="mt-m mb-xs semi-bold">Genre</h4>
+    <p>{book.genre}</p>
+    {/if}
+{/snippet}
+
+{#snippet editFields()}
+    <form>
+        <input class="input input-title mt-m mb-xs" bind:value={title} type="text" name="title" />
+        <div class="input-author">
+            <p>by</p>
+            <input type="text" class="input" bind:value={author} name="author"/>
+        </div>
+        <h4 class="mt-m mb-xs semi-bold">Your rating</h4>
+        <StarRating value={book.rating||0}/>
+        <p class="small-font">
+            Click to {book.rating ? "change": "give"} rating
+        </p>
+        <h4 class="mt-m mb-xs semi-bold">Description</h4>
+        <textarea class="textarea mb-m" name="desctiption" bind:value={description} placeholder="Give a description"></textarea>
+
+        {#if !book.finished_reading_on}
+        <Button isSecondary={true} onclick={() => console.log("Updating reading status")}>
+            {book.started_reading_on ? "I finished reading this book": "I started reading this book"}
+        </Button>
+        {/if}
+
+        <h4 class="mt-m mb-xs semi-bold">Genre</h4>
+        <input class="input" bind:value={genre} type="text" name="genre" />
+    </form>
 {/snippet}
 
 
@@ -57,7 +92,15 @@
     </button>
     <div class="book-container">
         <div class="book-info">
-            {@render bookInfo()}
+            {#if isEditMode}
+                {@render editFields()}
+            {:else}
+                {@render bookInfo()}
+            {/if}
+            <div class="button-container mt-m">
+                <Button isSecondary={true} onclick={toggleEditMode}>{isEditMode ? "Save changes": "Edit"}</Button>
+                <Button isDanger={true} onclick={() => console.log("delete the book")}>Delete book from library</Button>
+            </div>
         </div>
         <div class="book-cover">
             {#if book.cover_image}
@@ -87,7 +130,7 @@
         border: 1px solid black;
         border-radius: 15px;
         min-height: 400px;
-        max-width: 350px;
+        max-width: 450px;
         margin-left: 80px;
     }
 
@@ -96,5 +139,36 @@
         width: 100%;
         height: 100%;
         border-radius: inherit;
+    }
+
+    .add-cover {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .input {
+        padding: 8px 4px;
+        max-width: 100%;
+    }
+
+    .textarea {
+        width: 100%;
+    }
+
+    .input-title {
+        font-size: 60px;
+        font-weight: bold;
+        font-family: "EB Garamond", serif;
+    }
+
+    .input-author {
+        display: flex;
+        align-items: center;
+    }
+
+    .input-author p {
+        margin-right: 8px;
     }
 </style>
